8f34bce922255d0d0f7877f39ad63eae7b1b51ca
Marco Morais <marcoemorais@yahoo.com>
2022-09-11 13:55:53 -0700

    Replace cin with istringstream in wordcount

--- 12-algorithms/wordcount.cc
+++ 12-algorithms/wordcount.cc
@@ -5,13 +5,29 @@
 #include <string>
 #include <map>
 
-int main()
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+// wordcount returns word frequency count from stream.
+std::map<std::string, int>
+wordcount(std::istream& is)
 {
     std::map<std::string, int> wc;
-    std::for_each(std::istream_iterator<std::string>(std::cin),
+    std::for_each(std::istream_iterator<std::string>(is),
                   std::istream_iterator<std::string>(),
                   [&wc](const auto& s) { wc[s] += 1; });
-    for (const auto& [word, count] : wc) {
-        std::cout << word << ": " << count << '\n';
-    }
+    return wc;
+}
+
+TEST_CASE("[wordcount]")
+{
+    std::istringstream is{
+        "one two three four two three four three four four",
+    };
+    auto rcv = wordcount(is);
+    REQUIRE(rcv.size() == 4);
+    REQUIRE(rcv["one"] == 1);
+    REQUIRE(rcv["two"] == 2);
+    REQUIRE(rcv["three"] == 3);
+    REQUIRE(rcv["four"] == 4);
 }
